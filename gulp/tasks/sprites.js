dd1c23608a80c0e6a92125d496abca677fdbe49a
John Morris <johnmorris4@bellsouth.net>
2017-12-02 01:56:37 -0500

    finished sprite automation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
+del = require('del'),
 rename = require('gulp-rename');
 
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
             render: {
                 css: {
                     template:'./gulp/template/sprite.css'
@@ -14,11 +16,21 @@ var config = {
     }
 }
 
-gulp.task('createSprite', ()=>{
+gulp.task('beginClean', ()=>{
+    return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], ()=>{
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], ()=>{
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));  
+
+})
 
 gulp.task('copySpriteCSS',['createSprite'], ()=>{
     return gulp.src('./app/temp/sprite/css/*.css')
@@ -26,4 +38,8 @@ gulp.task('copySpriteCSS',['createSprite'], ()=>{
         .pipe(gulp.dest('./app/assets/styles/modules')); 
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteCSS']);
+gulp.task('endClean', ['copySpriteGraphic','copySpriteCSS'], ()=>{
+    return del('./app/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean','createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
