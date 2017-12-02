90ddf649baf3cd6ad53be794aaaf0b585f9c8562
John Morris <johnmorris4@bellsouth.net>
2017-12-02 04:12:37 -0500

    Finished Site Header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
+hexrgba = require('postcss-hexrgba'),
 mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba,autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
