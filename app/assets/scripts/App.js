8d23c6791b88cb1193780abe099d8fc90b24b13a
John Morris <johnmorris4@bellsouth.net>
2017-12-04 14:45:36 -0500

    finished modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
+
+var modal = new Modal(); 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,36 @@
+import $ from 'jquery';
+
+
+
+class Modal {
+    constructor() {
+        this.modalOpenButton = $(".open-modal");
+        this.modal = $(".modal");
+        this.closeModalBtn = $(".modal__close");
+        this.events();
+    }
+
+    events(){
+        //click open modal button
+        this.modalOpenButton.click(this.openModal.bind(this));
+        //pushed the escape key
+        $(document).keyup(this.keyPress.bind(this));
+        //clicking the close modal button
+        this.closeModalBtn.click(this.closeModal.bind(this));
+    }
+    keyPress(e) {
+        if(e.keyCode == 27 ) {
+            this.closeModal(); 
+        }
+    }
+
+    openModal(){
+        this.modal.addClass("modal--is-visible");
+        return false;
+    }
+    closeModal(){
+        this.modal.removeClass("modal--is-visible");
+    }
+}
+
+export default Modal;
