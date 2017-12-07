09877fe01e32ff452f1732ef9815ba3a652b91f2
John Morris <johnmorris4@bellsouth.net>
2017-12-07 04:52:54 -0500

    finished the modernizr sections

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6  */
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 import smoothScroll from 'jquery-smooth-scroll';
